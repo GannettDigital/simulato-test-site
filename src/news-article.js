b92a0790b6db1f1c1ce7cd36b94bcacd035319b9
scottgunther <sgunther@gannett.com>
2018-03-14 09:30:40 -0400

    issue-3: remove copyright image and use open svg

--- src/news-article.js
+++ src/news-article.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Image, Row, Col } from 'react-bootstrap';
-import NewsIcon from './news-icon.png';
+import { Row, Col } from 'react-bootstrap';
 import ViewStoryModal from './view-story-modal.js';
+
+// Below svg has the following license: https://fontawesome.com/license
 
 export default class NewsArticle extends React.Component {
   constructor(props) {
@@ -27,7 +28,7 @@ export default class NewsArticle extends React.Component {
       <div>
         <Row id={this.props.id} onClick={this.handleShow}>
           <Col xs={2} md={2}>
-            <Image id={`${this.props.id}Image`} responsive src={NewsIcon} alt="thumbnail" />
+            <svg id={`${this.props.id}Image`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M552 64H112c-20.858 0-38.643 13.377-45.248 32H24c-13.255 0-24 10.745-24 24v272c0 30.928 25.072 56 56 56h496c13.255 0 24-10.745 24-24V88c0-13.255-10.745-24-24-24zM48 392V144h16v248c0 4.411-3.589 8-8 8s-8-3.589-8-8zm480 8H111.422c.374-2.614.578-5.283.578-8V112h416v288zM172 280h136c6.627 0 12-5.373 12-12v-96c0-6.627-5.373-12-12-12H172c-6.627 0-12 5.373-12 12v96c0 6.627 5.373 12 12 12zm28-80h80v40h-80v-40zm-40 140v-24c0-6.627 5.373-12 12-12h136c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H172c-6.627 0-12-5.373-12-12zm192 0v-24c0-6.627 5.373-12 12-12h104c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12zm0-144v-24c0-6.627 5.373-12 12-12h104c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12zm0 72v-24c0-6.627 5.373-12 12-12h104c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12z"/></svg>
           </Col>
           <Col xs={10} md={10}>
             <Row>
